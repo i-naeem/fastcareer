6c6247caead8f8a38725b2592df80db49afaa498
Muhammad Naeem <inaeem@protonmail.com>
2023-06-20 12:34:54 +0500

    Add filter options

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,16 +1,24 @@
-import { HStack, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { HStack, Input, InputGroup, InputRightElement, Select } from '@chakra-ui/react';
 import { FaSearch as SearchIcon } from 'react-icons/fa';
+import { filterOptions } from '../constants';
 
 const Searchbar = props => {
   return (
     <form>
-      <HStack mb='2'>
-        <InputGroup>
-          <Input id='search-input' placeholder='Enter your search query...' />
-          <InputRightElement>
-            <SearchIcon />
-          </InputRightElement>
-        </InputGroup>
+      <InputGroup mb='2'>
+        <Input id='search-input' placeholder='Enter your search query...' />
+        <InputRightElement>
+          <SearchIcon />
+        </InputRightElement>
+      </InputGroup>
+      <HStack>
+        {filterOptions.map(filter => (
+          <Select placeholder={filter.placeholder} key={filter.id}>
+            {filter.options.map((option, idx) => (
+              <option key={idx}>{option}</option>
+            ))}
+          </Select>
+        ))}
       </HStack>
     </form>
   );
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,19 @@
 export const APP_NAME = 'AI Job Search System';
+
+export const filterOptions = [
+  {
+    id: 1,
+    placeholder: 'Category',
+    options: ['Product', 'Design', 'IT'],
+  },
+  {
+    id: 2,
+    placeholder: 'Work type',
+    options: ['Product', 'Design', 'IT'],
+  },
+  {
+    id: 3,
+    placeholder: 'Experience',
+    options: ['Expert', 'None', 'Junior'],
+  },
+];
