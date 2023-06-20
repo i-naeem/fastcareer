d3d8b55ac0fd879d5d276fbfb1563ef7287edbb6
Muhammad Naeem <inaeem@protonmail.com>
2023-06-20 11:43:15 +0500

    Update the heading and change do the work to search

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-import { Box, HStack, Heading, Image } from '@chakra-ui/react';
+import { Box, HStack, Heading, Image, Text } from '@chakra-ui/react';
 import Searchbar from '../components/Searchbar';
 import CatImage from '../assets/cat.png';
 
@@ -11,7 +11,13 @@ const Home = props => {
             <Heading size='3xl' mb='3'>
               Ready for a <br /> career change?
             </Heading>
-            <Heading size='xl'>Let us do the work for you</Heading>
+            <Heading size='xl'>
+              Let us{' '}
+              <Text as='span' color='red.400'>
+                search
+              </Text>{' '}
+              one for you
+            </Heading>
           </Box>
           <Box>
             <Image alt='A cat with laptop for decoration' src={CatImage} maxBlockSize='300' />
