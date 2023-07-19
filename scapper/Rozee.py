279dd2c72dff040689ca7c7719de45f0818b22b4
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-19 13:14:47 +0500

    Added `Rozee` scrapper

---
+++ scapper/Rozee.py
@@ -0,0 +1,27 @@
+import requests
+
+
+URL = "https://www.rozee.pk/services/job/jobSearch"
+PAYLOAD = dict([
+    ("userMeta[curSal]", "50000"),
+    ("userMeta[expSl]", "60000"),
+    ("userMeta[crl]", "693"),
+    ("userMeta[exp]", "719"),
+    ("userMeta[ct]", "2666"),
+    ("filters[q]", "all"),
+]
+)
+
+
+def get_posts():
+    response = requests.post(url=URL, data=PAYLOAD)
+    posts = response.json()['response']['jobs']['basic']
+
+    for post in posts:
+        yield dict(
+            title=post['title'],
+            location=post['city'],
+            company=post['company'],
+            last_date=post['applyBy'],
+            description=post["description"],
+        )
---
+++ scapper/__main__.py
@@ -0,0 +1,6 @@
+import Rozee
+
+
+if __name__ == '__main__':
+    for post in Rozee.get_posts():
+        print(post)
