3d0e01929434682424d77b5cba3cfeb58acfa412
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-19 11:50:50 +0500

    feat: added `crawl` method

--- scrapper/Crawler.py
+++ scrapper/Crawler.py
@@ -1,3 +1,4 @@
+import httpx
 
 
 class Crawler:
@@ -5,9 +6,14 @@ class Crawler:
         self.name = name
         self.start_urls = start_urls
 
+        self.visited = set()
+
+    def crawl(self):
+        for url in self.start_urls:
+            if url not in self.visited:
+                response = httpx.get(url)
+                self.visited.add(url)
+                yield response.content
+
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}(name={self.name!r}, start_urls={self.start_urls!r})'
-
-
-class Rozee(Crawler):
-    pass
