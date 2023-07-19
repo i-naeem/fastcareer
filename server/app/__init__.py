41c7098bbb8fb100039c51f2892c6d2f1db1253a
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-19 13:21:55 +0500

    feat: setup `views`

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="views")
 
     @app.route('/')
     def index_route():
-        return 'Hello World'
+        return render_template('index.jinja')
 
     return app
