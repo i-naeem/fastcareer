af1950e547404eb8a90c3d2d90ad65bc9ed56b0a
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-20 16:14:02 +0500

    Initialize `database`

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask import render_template
+from flask import Flask
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__, template_folder="views")
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://store.db"
+    db.init_app(app)
 
     @app.route('/')
     def index_route():
