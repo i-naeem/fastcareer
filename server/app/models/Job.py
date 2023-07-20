536c7fd9f9d2ca48fe4a41fab41a8ce8e7584d6a
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-20 16:28:15 +0500

    feat: created `Job Model`

---
+++ server/app/models/Job.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from app import db
+import sqlalchemy
+
+db: sqlalchemy
+
+
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    source = db.Column(db.String, nullable=False)
+    deadline = db.Column(db.Date, nullable=False)
+    company = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.Date, default=datetime)
+    description = db.Column(db.String, nullable=False)
