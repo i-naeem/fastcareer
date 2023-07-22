d8218f79f24abd6e98668b1b457c88e3c1ad8e5b
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-22 12:58:30 +0500

    feat: added `header`

--- client/src/routes/Root.js
+++ client/src/routes/Root.js
@@ -1,11 +1,17 @@
-import { Container, Heading } from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../components/ColorModeSwitcher';
+import { Box, Button, Container, Flex, Heading } from '@chakra-ui/react';
 
 export default function Root(props) {
   return (
     <Container maxW='container.xl'>
-      <header>
-        <Heading>FastCareer</Heading>
-      </header>
+      <Box as='header' p='4'>
+        <Flex>
+          <Heading me='auto'>FastCareer</Heading>
+
+          <Button>Home</Button>
+          <ColorModeSwitcher />
+        </Flex>
+      </Box>
       <main></main>
       <footer></footer>
     </Container>
