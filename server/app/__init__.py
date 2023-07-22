920c343d5822df75737e33c00c7447490c0b48c6
Muhammad Naeem Khan <inaeem@protonmail.com>
2023-07-22 10:43:40 +0500

    refactor: refactor `posts` route

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -5,16 +5,14 @@ db = SQLAlchemy()
 
 
 def create_app():
-    from .routes.api import api
-    from .routes.posts import posts_bp
+    from .routes.posts import posts
 
     app = Flask(__name__, template_folder="views")
 
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///store.db"
     db.init_app(app)
 
-    app.register_blueprint(blueprint=api, url_prefix="/api")
-    app.register_blueprint(blueprint=posts_bp)
+    app.register_blueprint(blueprint=posts, url_prefix="/posts")
 
     with app.app_context():
         db.create_all()
--- server/app/routes/posts.py
+++ server/app/routes/posts.py
@@ -1,12 +1,40 @@
+from sqlalchemy.exc import IntegrityError
 from flask import render_template
 from app.models.Job import Job
+from datetime import datetime
 from flask import Blueprint
+from flask import request
 from app import db
 
-posts_bp = Blueprint('posts', __name__)
+posts = Blueprint('/posts', __name__)
 
 
-@posts_bp.get('/posts')
-def posts():
+@posts.post('/posts')
+def create_post():
+    try:
+        d = request.get_json()
+        j = Job(
+            description=d['description'],
+            location=d['location'],
+            company=d['company'],
+            source=d['source'],
+            title=d['title'],
+
+            deadline=datetime.fromisoformat(d['deadline']),
+        )
+
+        db.session.add(j)
+        db.session.commit()
+
+        return dict(message="Job Added Successfully")
+
+    except IntegrityError as e:
+        return dict(message="the job already exists in the database"), 401
+    except Exception as e:
+        return dict(message=str(e)), 401
+
+
+@posts.get('/')
+def get_posts():
     results = db.session.execute(db.select(Job)).scalars()
     return render_template('posts.jinja', posts=results)
